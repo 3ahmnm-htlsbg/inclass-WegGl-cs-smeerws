699c05cfcd9d610db5888d62acefbcde261d1743
smeerws <hello@smeerws.at>
2020-01-17 10:07:26 +0100

    mod script and switch player

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    //public GameObject ballPosTopLeft;
+    //public GameObject ballPosTopRight;
+    //public GameObject ballPosBottomLeft;
+    //public GameObject ballPosBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject ball;
+
+    public void MoveTheBall()
+    {
+        Debug.Log(Random.Range(0,2));
+    }
+}
